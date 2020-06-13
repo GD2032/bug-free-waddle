5a84d3ff7a89ed669471c16b25911b91af00d9b3
gustavosalm <gustavosalmeida9821@gmail.com>
2020-06-13 17:29:24 -0300

    diálogo pronto e movimentação do player

---
+++ Assets/scripts/RafaBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RafaBehaviour : MonoBehaviour
+{
+    [SerializeField]private int speed;
+    private float hor, ver;
+    private Rigidbody2D rb;
+    private Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxisRaw("Horizontal");
+        ver = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(hor * speed, rb.velocity.y);
+    }
+}
--- Assets/scripts/TextBehaviour.cs
+++ Assets/scripts/TextBehaviour.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TextBehaviour : MonoBehaviour
 {
-    public GameObject texto, bGrnd;
+    public GameObject texto, textBox;
+    public GameObject[] screens;
     public string[] frases;
     private string display = "";
-    private int ind = 0, frase = 0;
+    private int ind = 0, frase = 0, isc = 0;
+    private float interval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,22 +25,48 @@ public class TextBehaviour : MonoBehaviour
         
     }
 
-    void Transit(bool black)
+    IEnumerator Transit(bool black)
     {
-        texto.GetComponent<Text>().text = "";
-        print(2);
-        if(black)
-            bGrnd.SetActive(true);
+        if(!black)
+        {
+            texto.GetComponent<Text>().alignment = TextAnchor.UpperLeft;
+            texto.GetComponent<Text>().fontSize = 45;
+            texto.GetComponent<RectTransform>().localPosition = new Vector3(19.5f, -182);
+            screens[0].SetActive(false);
+            screens[isc].SetActive(false); 
+            interval = 1.5f;
+            isc++; 
+        }
         else
-            bGrnd.SetActive(false);
+        {
+            texto.GetComponent<Text>().alignment = TextAnchor.UpperCenter;
+            texto.GetComponent<Text>().fontSize = 55;
+            texto.GetComponent<RectTransform>().localPosition = new Vector3(-1.6212f, -39);
+            screens[0].SetActive(true);
+            interval = 1f;
+        }        
+        textBox.SetActive(false);
+        texto.GetComponent<Text>().text = "";
+        print(2);          
+        if(frase != 4)
+        {
+            yield return new WaitForSeconds(1.5f);
+            textBox.SetActive(!black);
+            StartCoroutine("ShowText");
+        }      
+        else
+            SceneManager.LoadScene(2);
     }    
 
     IEnumerator ShowText()
-    {
+    {        
         for(int i = 0; i < frases[frase].Length; i++)
         {        
+            //frases[frase].Replace(";", "\n");
             yield return new WaitForSeconds(0.1f);
-            display += frases[frase][ind];
+            display = (frases[frase][ind].Equals(';')) ? display + "\n" : display + frases[frase][ind];
+            if(frases[frase][ind].Equals(';'))
+                yield return new WaitForSeconds(interval);                
             texto.GetComponent<Text>().text = display;
             if(ind + 1 == frases[frase].Length)
             {
@@ -45,8 +74,8 @@ public class TextBehaviour : MonoBehaviour
                 this.ind = 0;
                 display = "";
                 print(1);
-                yield return new WaitForSeconds(2);
-                Transit(false);
+                yield return new WaitForSeconds(2);                
+                StartCoroutine(Transit(frase == 2));
             }
             else
                 ind++;
