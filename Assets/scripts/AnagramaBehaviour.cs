45839f6169b0a1a33de074298db7d84bd01812ca
gustavosalm <gustavosalmeida9821@gmail.com>
2020-06-15 08:49:27 -0300

    começando os waypoints e o anagrama

---
+++ Assets/scripts/AnagramaBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class AnagramaBehaviour : MonoBehaviour
+{
+    public string word;
+    private char[] letras;
+    private List<int> sorteados = new List<int>();
+    private int s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        letras = word.ToCharArray();
+        s = Random.Range(0, word.Length);
+        for(int i = 0; i < word.Length; i++)
+        {            
+            while(sorteados.Contains(s))
+            {
+                s = Random.Range(0, word.Length);                
+            }
+            sorteados.Add(s);
+            transform.GetChild(i).gameObject.GetComponent<TextMeshPro>().text = word[s].ToString();        
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/RafaBehaviour.cs
+++ Assets/scripts/RafaBehaviour.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RafaBehaviour : MonoBehaviour
-{
-    [SerializeField]private int speed;
-    private float hor, ver;
-    private Rigidbody2D rb;
+{        
     private Transform tr;
+    private RaycastHit2D ray;
+    private GameObject currentWp;
+    public GameObject goBack;
     // Start is called before the first frame update
     void Start()
-    {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+    {        
         tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        hor = Input.GetAxisRaw("Horizontal");
-        ver = Input.GetAxisRaw("Vertical");
-
-        rb.velocity = new Vector3(hor * speed, rb.velocity.y);
+        ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity);
+        if(Input.GetKeyDown(KeyCode.Mouse0) && ray && ray.collider.gameObject.tag == "WayPoint")
+        {
+            print(1);
+            goBack.SetActive(true);
+            currentWp = ray.collider.gameObject;
+            currentWp.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Mouse0) && currentWp && ray && ray.collider.gameObject.name == "exit")
+        {
+            print(2);
+            currentWp.transform.GetChild(0).gameObject.SetActive(false);
+            currentWp = null;
+            goBack.SetActive(false);
+        }
     }
 }
